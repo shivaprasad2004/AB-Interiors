c458e0b2e35d670968f0a018fec8430be56026cf
shivaPrasad <shivaprasad040804@gmail.com>
2026-02-08 20:46:42 +0530

    Fixed App.jsx import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Inspiration from './components/Inspiration';
 import Process from './components/Process';
